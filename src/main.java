36bb821fda0df3b958bb577845c28cbba3e7ff35
Ünal ÖZKUL <59209507+unalozkul61@users.noreply.github.com>
2021-10-03 20:59:18 +0300

    Harmonik Sayı Hesaplama

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(" N sayisini Giriniz : ");
+         int n = input.nextInt();
+         double result = 0.0 ;
+
+
+         for ( double i = 1 ; i <= n ; i ++ ) {
+             result += (1/i);
+
+         }
+        System.out.println(result);
+
+
+    }
+}
